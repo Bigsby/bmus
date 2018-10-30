71425623afcfc8bc54d5c7bae77718bd2cb68ac1
Bruno Bernardo <bigsby@live.com>
2018-10-30 17:36:15 +0000

    Start adding scores

--- src/app/intervals/intervals.component.ts
+++ src/app/intervals/intervals.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from "@angular/core";
+import * as vf from "vexflow";
 import * as models from "./../models/models";
 import * as helpers from "./../models/helpers";
+import { startTimeRange } from "@angular/core/src/profile/wtf_impl";
+
 
 @Component({
     templateUrl: './intervals.component.html',
     styleUrls: ['./intervals.component.scss']
 })
-export class IntervalsComponent {
+export class IntervalsComponent implements AfterViewInit {
     allKeys: models.Key[] = helpers.getAllKeys();
     intervals: models.Interval[] = helpers.getIntervals();
     root: models.Key = this.allKeys[1];
@@ -18,7 +21,7 @@ export class IntervalsComponent {
     intervalDisplay: string;
     interval: models.Interval;
     tonalDistance: number;
-
+    @ViewChild("rootDiv")rootScore: ElementRef;
     constructor() {
         this.updateDetect();
         this.updateFind();
@@ -40,5 +43,24 @@ export class IntervalsComponent {
     updateFind() {
         this.findKey = helpers.getIntervalKey(this.findRoot, this.findInterval);
         this.findKeyDisplay = helpers.getKeyDisplay(this.findKey);
+    }
+
+    ngAfterViewInit(): void {
+        let renderer: vf.Flow.Renderer = new vf.Flow.Renderer(this.rootScore.nativeElement, vf.Flow.Renderer.Backends.SVG);
+        renderer.resize(400,200);
+        let context: vf.IRenderContext = renderer.getContext();
+        context.setFillStyle("white");
+        context.setStrokeStyle("white");
+        let stave:vf.Flow.Stave = new vf.Flow.Stave(10, 40, 400, { fill_style: "white" });
+        //stave.addClef("treble").addTimeSignature("4/4");
+        
+        stave.setContext(context).draw();
+
+        var voice = new vf.Flow.Voice({num_beats: 1,  beat_value: 4});
+        voice.addTickables([
+            new vf.Flow.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+        ]);
+        new vf.Flow.Formatter().joinVoices([voice]).format([voice], 400);
+        voice.draw(context, stave);
     }
 }
